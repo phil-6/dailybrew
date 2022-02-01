306b94cd4f016ca24ecd2a28e635c33ef80fc595
phil-6 <github@purpleriver.dev>
2022-02-01 14:47:25 +0000

    generate inventories model and controller
    rails g model Inventory user:belongs_to coffee:belongs_to -t rspec --no-jbuilder; rails g controller Inventories -t rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_144153) do
+ActiveRecord::Schema.define(version: 2022_02_01_144456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2022_02_01_144153) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["roaster_id"], name: "index_coffees_on_roaster_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.uuid "user_id", null: false
+    t.bigint "coffee_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["coffee_id"], name: "index_inventories_on_coffee_id"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -128,6 +137,8 @@ ActiveRecord::Schema.define(version: 2022_02_01_144153) do
   add_foreign_key "brews", "coffees"
   add_foreign_key "brews", "users"
   add_foreign_key "coffees", "roasters"
+  add_foreign_key "inventories", "coffees"
+  add_foreign_key "inventories", "users"
   add_foreign_key "reviews", "coffees"
   add_foreign_key "reviews", "users"
   add_foreign_key "roasters", "users"
