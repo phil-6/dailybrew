078b666b98e3d941d2f55a825b83aaae28a66121
phil-6 <github@purpleriver.dev>
2022-02-03 13:44:04 +0000

    skip authenticate user on roasters index

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -1,4 +1,5 @@
 class RoastersController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   before_action :set_roaster, only: %i[ show edit update destroy ]
 
   # GET /roasters or /roasters.json
