304752ba817995976c082a17b5d2569918b939cd
phil-6 <github@purpleriver.dev>
2022-02-03 08:24:10 +0000

    comment configure permitted params for future work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protected
