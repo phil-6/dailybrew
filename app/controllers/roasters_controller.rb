620f638b73c8bb51f133bb5432a9583e1a6cda32
phil-6 <github@purpleriver.dev>
2022-02-08 07:41:26 +0000

    remove debugging 'puts'

--- app/controllers/roasters_controller.rb
+++ app/controllers/roasters_controller.rb
@@ -67,7 +67,6 @@ class RoastersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_roaster
-      puts params
       @roaster = Roaster.find(params[:id])
     end
 
