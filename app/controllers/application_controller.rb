73ab0b67a4446177a905f4d2322c67862083e00b
phil-6 <github@purpleriver.dev>
2022-02-09 15:15:43 +0000

    set up pagination for roasters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  include Pagy::Backend
   # before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
