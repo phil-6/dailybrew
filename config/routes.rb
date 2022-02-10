6a0eb3fd7d52e7beb3734f2963c5aa37fc92b0c2
garethdandrews <garethdandrews@gmail.com>
2022-02-10 21:04:44 +0000

    Added a new page for terms to live

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#main'
   get 'about', to: 'pages#about'
   get 'subscription', to: 'pages#subscription'
+  get 'terms', to: 'pages#terms'
 
   devise_for :users
 
