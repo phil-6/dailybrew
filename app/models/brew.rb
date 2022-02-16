707ac51a990b174139b070ce671863d793d63838
phil-6 <github@purpleriver.dev>
2022-02-16 13:33:49 +0000

    add live update to recent brews on new brew created

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -7,4 +7,14 @@ class Brew < ApplicationRecord
   counter_culture :coffee, column_name: proc { |model| model.public? ? 'public_brews_count' : nil }
 
   scope :visible, -> { where(public: true) }
+
+  after_create_commit do
+    if public
+      broadcast_prepend_to(
+        'recent_brews',
+        target: "brews_coffee_#{coffee.id}",
+        locals: { brew: self }
+      )
+    end
+  end
 end
