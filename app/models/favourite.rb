f0554d36d49353d50c8428084cdf898f3a100f9a
phil-6 <github@purpleriver.dev>
2022-02-16 08:17:29 +0000

    broadcast changes to favourite count on update

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -4,5 +4,15 @@ class Favourite < ApplicationRecord
   counter_culture :user
   counter_culture :coffee
 
-  validates :coffee, uniqueness: {scope: :user_id}
+  validates :coffee, uniqueness: { scope: :user_id }
+
+  after_commit do
+    broadcast_update_to(
+      'favourites',
+      partial: 'favourites/favourites_count',
+      target: "favourites_count_coffee_#{coffee.id}",
+      plain: Favourite.where(coffee: coffee).count,
+      locals: { coffee: coffee }
+    )
+  end
 end
