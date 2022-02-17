0ec9e54661ad868b65bc846c10c9a1729d342f9a
phil-6 <github@purpleriver.dev>
2022-02-17 16:27:09 +0000

    add scope on roasters for roasters with coffees

--- app/models/roaster.rb
+++ app/models/roaster.rb
@@ -4,7 +4,10 @@ class Roaster < ApplicationRecord
   validates :website, uniqueness: { case_sensitive: false }
   validates :reference, uniqueness: true
 
+  scope :with_coffees, -> { joins(:coffees).distinct }
+
   def update_coffees
     UpdateCoffeesJob.perform_later(reference)
   end
+
 end
