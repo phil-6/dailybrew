8683969379372e860b548eec53987777042bb29a
phil-6 <github@purpleriver.dev>
2022-02-17 08:35:51 +0000

    show all favourites and shelf items on user dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    @shelf_coffees = current_user.shelf_coffees.last(5)
-    @favourites = current_user.favourite_coffees.last(5)
+    @shelf_coffees = current_user.shelf_coffees
+    @favourites = current_user.favourite_coffees
     @brews = current_user.brews.order('created_at DESC').includes(:coffee).first(10)
   end
 end
