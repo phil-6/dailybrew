80ee3164fc5c35c07af6aaa3576257ee19a1d603
garethdandrews <garethdandrews@gmail.com>
2022-02-22 18:58:11 +0000

    Tidied up JS

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,20 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
+const navbar = document.querySelector('nav'),
+      navMenu = navbar.querySelector('.nav-menu'),
+      navMenuBackground = navbar.querySelector('.nav-menu-background')
+
 export default class extends Controller {
     expand() {
-        const navbar = this.element,
-              navMenu = navbar.querySelector('.nav-menu'),
-              navMenuBackground = navbar.querySelector('.nav-menu-background')
-
         navMenu.style.display = 'flex'
         navMenuBackground.style.display = 'block'
     }
 
     collapse() {
-        const navbar = this.element,
-              navMenu = navbar.querySelector('.nav-menu'),
-              navMenuBackground = navbar.querySelector('.nav-menu-background')
-
         navMenu.style.display = 'none'
         navMenuBackground.style.display = 'none'
     }
