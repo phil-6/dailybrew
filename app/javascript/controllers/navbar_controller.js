f72946a04dea4f3ed6af0442b8a773e6a101813c
garethdandrews <garethdandrews@gmail.com>
2022-02-22 17:35:51 +0000

    Changed the navbar so on smaller screens the nav links are hidden and will be contained in an expandable container

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,13 +4,8 @@ export default class extends Controller {
 
     expand() {
         const navbar = this.element
-        const expandBtn = navbar.querySelector('.nav-left-mobile .btn.btn-primary-border')
-        const iconClassList = expandBtn.querySelector('i').classList
+        // const iconClassList = expandBtn.querySelector('i').classList
 
-        if (navbar.classList.toggle('expanded')) {
-            iconClassList.replace('la-bars', 'la-times')
-        } else {
-            iconClassList.replace('la-times', 'la-bars')
-        }
+        
     }
 }
