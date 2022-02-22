46b2e4497e31d2aadd246ca81a04a8c9a9ef4dd0
phil-6 <github@purpleriver.dev>
2022-02-22 13:13:51 +0000

    add scraper for pilgrims coffee

---
+++ app/services/fetch_coffees/pilgrims_coffee.rb
@@ -0,0 +1,54 @@
+module FetchCoffees
+  class PilgrimsCoffee
+    require 'open-uri'
+    require 'net/http'
+    require 'json'
+    require 'nokogiri'
+
+    def scrape
+      @coffees = []
+      roaster_url_root = 'https://www.pilgrimscoffee.com/'
+      coffee_index_url = 'collections/frontpage'
+      coffee_css_on_index = '.prod-caption a'
+      exclude_types = %w[tea sample gift]
+
+      coffee_index_html = Net::HTTP.get_response(URI.parse(roaster_url_root + coffee_index_url))
+      coffee_index_page = Nokogiri::HTML(coffee_index_html.body)
+
+      coffee_index_page.css(coffee_css_on_index).each do |coffee_section|
+        coffee = {}
+        coffee['url'] = coffee_section[:href]
+        next if exclude_types.any? { |s| coffee['url'].include? s }
+
+        puts coffee['url']
+        coffee_html = Net::HTTP.get_response(URI.parse(roaster_url_root + coffee['url']))
+        coffee_page = Nokogiri::HTML(coffee_html.body)
+
+        coffee['name'] = coffee_page.css('h1.product-title').text
+
+        coffee_page.css('#product-description table tr').each do |row|
+          if row.css('td').text.downcase.include? 'location'
+            coffee['region'] = row.css('td').last.text
+          elsif row.css('td').text.downcase.include? 'altitude'
+            coffee['altitude'] = row.css('td').last.text.sub(/\n/, '').sub(/\s.+/, '')
+          elsif row.css('td').text.downcase.include? 'owner'
+            coffee['producer'] = row.css('td').last.text
+          elsif row.css('td').text.downcase.include? 'process'
+            coffee['process'] = row.css('td').last.text
+          elsif %w[variety varietal].any? { |s| row.css('td').text.downcase.include? s }
+            coffee['variety'] = row.css('td').last.text
+          elsif row.css('td').text.downcase.include? 'taste'
+            coffee['tasting_notes'] = row.css('td').last.text
+          end
+        end
+
+        coffee['available'] = true
+
+        @coffees << coffee
+      end
+
+      #puts @coffees.to_json
+      @coffees
+    end
+  end
+end
