a29c287b95f679de3aac494d4a861fee01312e26
phil-6 <github@purpleriver.dev>
2022-02-24 07:34:50 +0000

    add some stats to admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,26 @@
 # app/controllers/admin_controller.rb
 class AdminController < ApplicationController
   before_action :authorize_admin
-  before_action :users
+  before_action :stats, only: :dashboard
 
   # GET /admin/dashboard/
   def dashboard
-    @pagy, @users = pagy(@users.order('created_at DESC'), items: 10)
+    @pagy, @users = pagy(User.all.order('created_at DESC'), items: 10)
+
     render 'dashboard'
   end
 
   private
 
-  def users
-    @users = User.first(10)
+  def stats
+    @stats = {}
+    @stats['user_count'] = User.count
+    @stats['roaster_count'] = Roaster.count
+    @stats['roaster_with_coffees'] = Roaster.with_coffees.count
+    @stats['coffee_count'] = Coffee.count
+    @stats['coffee_with_brews'] = Coffee.joins(:brews).distinct.count
+    @stats['brews_count'] = Brew.count
+    @stats['daily_brews'] = Brew.today.count
+    @stats['user_with_brews'] = User.joins(:brews).distinct.count
   end
 end
