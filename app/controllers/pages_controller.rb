a9650fedb643e8306cfe60b44be1ae4f7c15ae68
Phil Reynolds <github@purpleriver.dev>
2022-02-26 18:21:43 +0000

    add lastest user to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :render_content
+  before_action :newest_user, only: :main
 
   private
+
+  def newest_user
+    @newest_user = User.last
+  end
 
   def render_content
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true)
