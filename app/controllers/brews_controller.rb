df7315221f23015a9f87c18e802aa772e7e5e755
garethdandrews <garethdandrews@gmail.com>
2022-03-03 16:12:25 +0000

    Show newest brews at top of table. Show actual date if created at more than a week ago

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -4,7 +4,7 @@ class BrewsController < ApplicationController
 
   # GET /brews or /brews.json
   def index
-    @brews = params[:coffee_id] ? Coffee.find(params[:coffee_id]).brews : current_user.brews.all
+    @brews = (params[:coffee_id] ? Coffee.find(params[:coffee_id]).brews : current_user.brews.all).order(created_at: :desc)
   end
 
   # GET /brews/1 or /brews/1.json
