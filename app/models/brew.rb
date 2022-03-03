e6e5f66ec0856857b85bae6a84c3cf53df77d357
garethdandrews <garethdandrews@gmail.com>
2022-03-03 17:16:19 +0000

    Formatted time method on brew model

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -42,4 +42,10 @@ class Brew < ApplicationRecord
       html: ActionController::Base.helpers.pluralize(Brew.today.count, 'coffee')
     )
   end
+
+  def formatted_time
+    mins = time / 60
+    secs = time - (mins * 60)
+    format('%<mins>02d:%<secs>02d', { mins:, secs:, ms: 0 })
+  end
 end
